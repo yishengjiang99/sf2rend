ad4fbab26428bc4d32e2abae326e3fcb2c3da8ac
Yisheng Jiang <yishengjiang@yahoo.com>
2021-08-10 12:11:18 -0700

    s

---
+++ midiworker.js
@@ -0,0 +1,35 @@
+import { scheduler } from "./midiread/dist/scheduler.js";
+(async () => {
+  const url = self.location.hash.split("#")[1];
+  console.log(url);
+  const res = await fetch(url);
+  const ab = await res.arrayBuffer();
+  const {
+    ctrls: { run, rwd, pause },
+    totalTicks,
+    tracks,
+    presets,
+  } = await scheduler(new Uint8Array(ab), postMessage);
+  console.log(res);
+  // @ts-ignore
+  postMessage({ totalTicks, tracks, presets });
+  onmessage = ({ data: { cmd, amt } }) => {
+    switch (cmd) {
+      case "start":
+        run();
+        break;
+      case "pause":
+        pause();
+        break;
+      case "resume":
+        run();
+        break;
+
+      case "rwd":
+        rwd(amt || 16);
+        break;
+      case "ff":
+        break;
+    }
+  };
+})();
