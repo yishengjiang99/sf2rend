c4b0aa998869953a14b943edeba74e8eb0156bfa
Yisheng Jiang <yishengjiang@yahoo.com>
2021-08-17 10:05:58 -0700

    a
    a

--- sf2-service/read.js
+++ sf2-service/read.js
@@ -96,8 +96,7 @@ export function loadProgram(
       20,
       5
     );
-    start = Math.max(startloop - 56, start);
-
+    start = Math.max(start, startloop - 24);
     const [originalPitch] = new Uint8Array(dv, 20 + 5 * 4, 1);
     const range = [sdtaStart + start * 2, sdtaStart + end * 2 + 1];
     //      "bytes=" + (sdtaStart + start * 2) + "-" + (sdtaStart + end * 2 + 1);
