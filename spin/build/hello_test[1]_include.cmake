8158de11926778fdcb7ce967468d2ecf2c90c9d0
Yisheng Jiang <yishengjiang@yahoo.com>
2021-08-22 20:25:44 -0700

    s

--- spin/build/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/yisheng/Documents/GitHub/sf2rend/spin/build/hello_test[1]_tests.cmake")
-  include("/Users/yisheng/Documents/GitHub/sf2rend/spin/build/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
