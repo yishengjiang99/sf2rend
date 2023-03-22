9a1a86aaa2801363c091ba663291a9bd62cc81f4
Yisheng Jiang <yisheng.jiang@gmail.com>
2023-03-22 08:07:10 -0700

    s

--- e2e.js
+++ e2e.js
@@ -1,17 +1,32 @@
-import { mkcanvas, chart } from "https://unpkg.com/mk-60fps@1.1.0/chart.js";
+import {
+  mkcanvas,
+  chart,
+  renderFrames,
+} from "https://unpkg.com/mk-60fps@1.1.0/chart.js";
 import { mkdiv } from "https://unpkg.com/mkdiv@3.1.0/mkdiv.js";
 import { SpinNode } from "./spin/spin.js";
 import { mkpath } from "./src/path.js";
 import SF2Service from "./sf2-service/index.js";
+import { newSFZone } from "./sf2-service/zoneProxy.js";
 const sf2url = "file.sf2";
 
 const sf2file = new SF2Service(sf2url);
 const loadWait = sf2file.load();
 
-let program, spinner, zref, presetId;
+let program,
+  spinner,
+  zone,
+  presetId,
+  articleMain,
+  canvas,
+  main,
+  rightPanel,
+  volEGCanvas,
+  modEGCanvas;
 
 renderMain();
-window.addEventListener("hashchange", renderMain);
+window.addEventListener("hashchange", rendProgram);
+
 async function renderMain() {
   await loadWait;
   document.body.innerHTML = "";
@@ -26,35 +41,39 @@ async function renderMain() {
       )
     )
   );
-  const [leftNav, rightPanel] = [
-    mkdiv(
-      "section",
-      {
-        class: "col sidebar",
-      },
-      [
-        mkdiv(
-          "section",
-          { class: "sidebar-header" },
-          sf2file.url.split("/").pop()
-        ),
-        mkdiv("nav", {}, progList),
-      ]
-    ),
-    mkdiv("div", { class: "col note-viewer" }),
-  ];
-  const main = mkdiv("div", { class: "main" }, [leftNav, rightPanel]);
+  rightPanel = mkdiv("div", { class: "col note-viewer" });
+  const leftNav = mkdiv(
+    "section",
+    {
+      class: "col sidebar",
+    },
+    [
+      mkdiv("button", { onclick: () => rendSample() }, "play"),
+      mkdiv(
+        "section",
+        { class: "sidebar-header" },
+        sf2file.url.split("/").pop()
+      ),
+      mkdiv("nav", {}, progList),
+    ]
+  );
+  main = mkdiv("div", { class: "main" }, [leftNav, rightPanel]);
+  document.body.append(main);
+  await startSpinner();
+  // rendProgram();
+}
+
+function rendProgram() {
   const presetId = document.location.hash.substring(1).split("|");
   const intpre = parseInt(presetId);
   const pid = intpre;
   const bid = 0;
   program = sf2file.loadProgram(pid, bid);
-  console.log(program.zMap);
-  if (!zref) zref = program.filterKV(55, 55)[0]?.zref;
+  if (!zone) zone = program.filterKV(55, 55)[0];
 
   const kRangeList = program.zMap.map(
     (z) =>
-      `<option value=${z.ref} ${z.ref + "" == zref ? "selected" : ""}>${
+      `<option value=${z.ref} ${z.ref + "" == zone.ref ? "selected" : ""}>${
         z.Unused1 + "|" + z.Unused2
       } ${
         "key " +
@@ -70,84 +89,145 @@ async function renderMain() {
       mkdiv(
         "select",
         {
-          oninput: (e) => renderZ(e.target.value),
+          oninput: (e) =>
+            renderZ(program.zMap.filter((z) => e.target.value)[0]),
         },
         kRangeList
       )
     ),
   ]);
-  const articleMain = mkdiv("div", { class: "note-preview" }, [
+  articleMain = mkdiv("div", { class: "note-preview" }, [
     mkdiv("div", {
       style:
         "display:flex flex-direction:row; max-height:50vh; overflow-y:scroll; gap:0 20px 20px",
     }),
-  ]).attachTo(main);
+  ]);
   const mainRight = mkdiv("div", { class: "note" }, [
     mkdiv("div", { class: "note-title" }, [sf2file.programNames[presetId]]),
     articleHeader,
-    ,
-  ]).attachTo(rightPanel);
-  document.body.append(main);
-  const canvas = mkcanvas({ container: articleHeader });
+  ]);
+  rightPanel.replaceChildren(mainRight, articleMain);
 
-  renderZ(zref);
-  async function renderZ(zref) {
-    let zoneSelect = zref
-      ? program.zMap.find((z) => z.ref + "" == zref)
-      : program.zMap[0];
-    articleMain.innerHTML = 0;
+  canvas = mkcanvas({ container: articleHeader });
+  renderZ(zone);
+  spinner.shipProgram(program, intpre);
+}
+async function renderZ(zoneSelect) {
+  if (!zoneSelect) {
+    return;
+  }
+  const zoneObj = newSFZone(zoneSelect.arr);
 
-    if (zoneSelect) {
-      const zattrs = Object.entries(zoneSelect).filter(
-        ([attr, val], idx) => idx < 60
-      );
+  const zattrs = Object.entries(zoneObj).filter(([attr, val], idx) => idx < 60);
 
-      const pcm = await zoneSelect.shdr.data();
-      chart(canvas, pcm);
+  const pcm = await zoneSelect.shdr.data();
+  chart(canvas, pcm);
 
-      const zoneinfo = mkdiv("div", [
-        mkdiv("div", [
-          "smpl: ",
-          zoneSelect.shdr.SampleId,
-          " ",
-          zoneSelect.shdr.name,
-          "<br>nsample: ",
-          zoneSelect.shdr.nsamples,
-          "<br>srate: " + zoneSelect.shdr.originalPitch,
-          "<br>Range: ",
-          zoneSelect.shdr.range.join("-"),
-          "<br>",
-          "loop: ",
-          zoneSelect.shdr.loops.join("-"),
-          "<br>",
+  const zoneinfo = mkdiv("div", [
+    mkdiv("div", [
+      "smpl: ",
+      zoneSelect.shdr.SampleId,
+      " ",
+      zoneSelect.shdr.name,
+      "<br>nsample: ",
+      zoneSelect.shdr.nsamples,
+      "<br>srate: " + zoneSelect.shdr.originalPitch,
+      "<br>Range: ",
+      zoneSelect.shdr.range.join("-"),
+      "<br>",
+      "loop: ",
+      zoneSelect.shdr.loops.join("-"),
+      "<br>",
 
-          JSON.stringify(zoneSelect.KeyRange),
-          "<br>",
-          JSON.stringify(zoneSelect.VolRange),
-        ]),
-        ..."Sustain,Attenuation,VolEnv,Filter,LFO".split(",").map((keyword) =>
-          mkdiv(
-            "div",
-            { style: "padding:10px;color:gray;" },
-            zattrs
-              .filter(([k]) => k.includes(keyword))
-              .map(([k, v]) => k + ": " + v)
-              .join("<br>")
-          )
-        ),
-      ]);
-      zoneinfo.attachTo(articleMain);
+      JSON.stringify(zoneSelect.KeyRange),
+      "<br>",
+      JSON.stringify(zoneSelect.VolRange),
+    ]),
+    ..."Attenuation,VolEnv,Filter,LFO"
+      .split(",")
+      .map((keyword) => renderArticle(keyword, zoneSelect)),
+  ]);
+
+  articleMain.replaceChildren(zoneinfo);
+
+  //drawEV(zone.arr.slice(33, 39), volEGCanvas);
+
+  function renderArticle(keyword, zone) {
+    let canvas;
+    const zoneObj = newSFZone(zone.arr);
+    const zattrs = Object.entries(zone)
+      .filter(([attr, val], idx) => idx < 60)
+      .filter(([k]) => k.includes(keyword));
+
+    const attrVals = mkdiv(
+      "ul",
+      zattrs.map(([k, v]) =>
+        mkdiv("li", [
+          k,
+          ": ",
+          mkdiv("label", [v]),
+          mkdiv("input", {
+            type: "range",
+            min: -12000,
+            max: 5000,
+            step: 10,
+            value: v,
+            oninput: (e) => {
+              e.target.parentElement.querySelector("label").textContent =
+                e.target.value;
+              zoneObj[k] = e.target.value;
+              drawEV(zoneObj, canvas);
+            },
+          }),
+        ])
+      )
+    );
+    const details = mkdiv("div");
+    const article = mkdiv("article", [attrVals, details]);
+    if (keyword === "VolEnv") {
+      canvas = mkcanvas({ container: details, title: "amp eg" });
+      drawEV(zoneObj, canvas);
     }
+    return article;
   }
 }
-const button = mkdiv("button", "start");
-button.attachTo(document.body);
-button.addEventListener("click", start, { once: true });
-async function start() {
-  const ctx = new AudioContext();
-  if (ctx.state == "suspended") {
-    await ctx.resume();
-  }
-  spinner = (await mkpath(ctx)).spinner;
-  spinner.shipProgram(program, presetId);
+let ctx;
+async function startSpinner() {
+  ctx = ctx || new AudioContext();
+  if (!spinner) spinner = await mkpath(ctx);
 }
+async function rendSample() {
+  if (ctx.state !== "running") await ctx.resume();
+
+  spinner.port.postMessage([
+    0x90,
+    0,
+    zone.ref,
+    zone.calcPitchRatio(55, spinner.context.sampleRate),
+    122,
+  ]);
+}
+const drawEV = async (zone, target) => {
+  const [delay, att, hold, decay, sustain, release] = zone.arr.slice(33, 39);
+  console.log(delay, att, hold, decay, sustain, release);
+  const tc2time = (t) => Math.pow(2, t / 1200);
+  const ctx = new OfflineAudioContext(1, 6000, 3000);
+  const amp = new GainNode(ctx, { gain: 0 });
+  const o = new ConstantSourceNode(ctx, { offset: 1 });
+  amp.gain.linearRampToValueAtTime(1, tc2time(att) + tc2time(delay));
+  amp.gain.cancelAndHoldAtTime(tc2time(att) + tc2time(delay));
+  amp.gain.setTargetAtTime(
+    sustain / 10 / 1400,
+    tc2time(att) + tc2time(delay) + tc2time(hold),
+    1 / tc2time(decay)
+  );
+  amp.gain.cancelAndHoldAtTime(
+    tc2time(att) + tc2time(delay) + tc2time(hold) + 0.1
+  );
+  amp.gain.setTargetAtTime(0, 0.7, 1 / tc2time(release));
+  o.connect(amp);
+  amp.connect(ctx.destination);
+  o.start(0);
+  const rendbuff = await ctx.startRendering();
+  chart(target, rendbuff.getChannelData(0));
+};
