7362759d0c9f917f64b475a7956671e3e1acfb4c
Yisheng Jiang <yishengj@juniper.net>
2024-07-21 21:01:01 -0700

    a

--- spin/src/spin.c
+++ spin/src/spin.c
@@ -2,54 +2,252 @@
 
 #include "spin.h"
 
+#include "calc.h"
+
 #define MAX_VOICE_CNT 256
 
 // ghetto malloc all variables
 int sp_idx = 0;
 spinner sps[MAX_VOICE_CNT];
-
 pcm_t pcms[4096];
-unsigned char midi_cc_vals[nmidiChannels * 128] = {0};
+uint8_t midi_cc_vals[nmidiChannels * 128] = {0};
 float outputs[MAX_VOICE_CNT * RENDQ * 2];
-
 float silence[440] = {.0f};
 float calc_pitch_diff_log(zone_t *z, pcm_t *pcm, unsigned char key);
-int output_arr_len = MAX_VOICE_CNT * RENDQ * 2;
 float volEgOut[RENDQ];
 float modEgOut[RENDQ];
 float lfo1Out[RENDQ];
 float lfo2Out[RENDQ];
-#define effect_floor(v) v <= -12000 ? 0 : calcp2over1200(v)
 
-void sp_wipe_output_tab()
-{
-  for (int i = 0; i < output_arr_len; i++)
-  {
+void sp_wipe_output_tab() {
+  int output_arr_len = MAX_VOICE_CNT * RENDQ * 2;
+  for (int i = 0; i < output_arr_len; i++) {
     outputs[i] = 0.0f;
   }
 }
+
 spinner *spRef(int idx) { return &sps[idx]; }
 pcm_t *pcmRef(int sampleId) { return &pcms[sampleId]; }
-spinner *allocate_sp()
-{
+
+spinner *allocate_sp() {
   spinner *x = &sps[sp_idx % MAX_VOICE_CNT];
   x->outputf = &outputs[sp_idx * RENDQ * 2];
   sp_idx++;
   return x;
 }
+#define def_drum_c 9
+#define modulo_s16f_inverse 1.0f / 32767.1f
+#define modulo_u16f (float)(((1 << 16) + .1f))
+extern float tanf(float t);
+float dummy[666];  // backward compact hack
 
-spinner *newSpinner(int ch)
-{
+void scaleTc(EG *eg, unsigned int pcmSampleRate) {
+  float scaleFactor = SAMPLE_RATE / (float)pcmSampleRate;
+  eg->attack *= scaleFactor;
+  eg->delay *= scaleFactor;
+  eg->decay *= scaleFactor;
+  eg->release *= scaleFactor;
+  eg->hold *= scaleFactor;
+}
+
+void init_vol_eg(EG *eg, zone_t *z, unsigned int pcmSampleRate) {
+  float scaleFactor = SAMPLE_RATE / (float)pcmSampleRate;
+  eg->attack = z->VolEnvAttack * scaleFactor;
+  eg->delay = z->VolEnvDelay * scaleFactor;
+  eg->decay = z->VolEnvDecay * scaleFactor;
+  eg->release = z->VolEnvRelease * scaleFactor;
+  eg->hold = z->VolEnvHold * scaleFactor;
+  eg->sustain = z->VolEnvSustain * scaleFactor;
+  eg->stage = init;
+  eg->nsteps = 0;
+}
+
+void init_mod_eg(EG *eg, zone_t *z, unsigned int pcmSampleRate) {
+  eg->attack = z->ModEnvAttack;
+  eg->delay = z->ModEnvDelay;
+  eg->decay = z->ModEnvDecay;
+  eg->release = z->ModEnvRelease;
+  eg->hold = z->ModEnvHold;
+  eg->sustain = z->ModEnvSustain;
+}
+
+void new_lpf(Biquad *biq, float fc) {
+  double K = tanf(3.1415f * fc);
+  double KK = K * K;
+  double norm = 1.0 / (1 + K * biq->QInv + KK);
+  biq->a0 = KK * norm;
+  biq->a1 = 2 * biq->a0;
+  biq->b1 = 2 * (KK - 1) * norm;
+  biq->b2 = (1 - K * biq->QInv + KK) * norm;
+}
+
+float calc_lpf(Biquad *b, double In) {
+  double Out = In * b->a0 + b->z1;
+  b->z1 = In * b->a1 + b->z2 - b->b1 * Out;
+  b->z2 = In * b->a0 - b->b2 * Out;
+  return (float)Out;
+}
+
+float LFO_roll_out(LFO *lfo, unsigned int n, float *output) {
+  while (n--) {
+    if (lfo->delay > 0) {
+      lfo->delay--;
+      *output++ = 0.f;
+      continue;
+    } else {
+      lfo->phase += lfo->phaseInc;
+      *output++ = (float)(((short)lfo->phase) * modulo_s16f_inverse);
+    }
+  }
+  return *output;
+}
+
+void LFO_set_frequency(LFO *lfo, short ct) {
+  double freq = timecent2hertz(ct);
+  lfo->phaseInc = (unsigned short)(modulo_u16f * freq / SAMPLE_RATE);
+}
+
+float LFO_roll(LFO *lfo, unsigned int n) {
+  if (lfo->delay > n) {
+    lfo->delay -= n;
+    return 0.0f;
+  } else {
+    n -= lfo->delay;
+    lfo->delay = 0;
+  }
+  while (n--) lfo->phase += lfo->phaseInc;
+  float lfoval = (float)(((short)lfo->phase) * modulo_s16f_inverse);
+  return lfoval;
+}
+
+void advanceStage(EG *eg);
+float update_eg(EG *eg, int n);
+
+void eg_roll(EG *eg, int n, float *output) {
+  while (n-- && eg->nsteps--) {
+    if (eg->stage == attack) {
+      int lut_index = fixed_floor(eg->progress);
+      double frag = get_fraction(eg->progress);
+      double f1 = att_db_levels[lut_index], f2 = att_db_levels[lut_index + 1];
+      eg->egval = lerpd(f1, f2, frag);
+    } else {
+      eg->egval += eg->egIncrement;
+    }
+    *output++ = eg->egval;
+  }
+  if (eg->egval > 0) eg->egval = 0.0f;
+  if (eg->nsteps <= 7) advanceStage(eg);
+}
+/**
+ * advances envelope generator by n steps..
+ * shift to next stage and advance the remaining n steps
+ * if necessary
+ *
+ */
+float update_eg(EG *eg, int n) {
+  while (n--) {
+    eg->egval += eg->egIncrement;
+    eg->nsteps--;
+  }
+  if (eg->nsteps <= 7) advanceStage(eg);
+  if (eg->egval > 0) eg->egval = 0.0f;
+  return eg->egval;
+}
+
+void advanceStage(EG *eg) {
+  switch (eg->stage) {
+    case inactive:
+      eg->stage++;
+      return;
+    case init:
+      eg->stage = delay;
+      eg->egval = MAX_EG;
+
+      if (eg->delay > -12000) {
+        eg->egval = MAX_EG;
+        eg->nsteps = timecent2sample(eg->delay);
+        eg->egIncrement = 0.0f;
+        break;
+      }
+    case delay:
+      eg->stage = attack;
+      eg->egval = MAX_EG;
+      eg->nsteps = timecent2sample(eg->attack);
+      eg->progress = double2fixed(0);
+      eg->progressInc = double2fixed(255.0 / (double)eg->nsteps);
+      break;
+    case attack:
+      eg->stage = hold;
+      eg->egval = 0.0f;
+      eg->nsteps = timecent2sample(eg->hold);
+      eg->egIncrement = 0.0f;
+
+      break;
+    case hold: /** TO DECAY */
+      eg->stage = decay;
+
+      if (eg->sustain > 0) {
+        eg->nsteps = timecent2sample(eg->decay);
+        eg->egIncrement = (MAX_EG + eg->sustain / 1000) / eg->nsteps;
+
+      } else {
+        eg->nsteps = 0;
+      }
+      break;
+
+    case decay:  // headsing to released;
+
+      /*
+      37 sustainVolEnv This is the decrease in level, expressed in centibels,
+      to which the Volume Envelope value ramps during the decay phase. For the
+      Volume Envelope, the sustain level is best expressed in centibels of
+      attenuation from full scale. A value of 0 indicates the sustain level is
+      full level; this implies a zero duration of decay phase regardless of
+      decay time. A positive value indicates a decay to the corresponding
+      level. Values less than zero are to be interpreted as zero;
+      conventionally 1000 indicates full attenuation. For example, a sustain
+      level which corresponds to an absolute value 12dB below of peak would be
+      120.*/
+      eg->stage = sustain;
+      eg->egIncrement = 0.0f;
+      eg->nsteps = 0xfffff;
+      break;
+
+      // sustain = % decreased during decay
+
+    case sustain:
+      eg->stage = release;
+      int stepsFull =
+          timecent2sample(eg->release);  //+ timecent2sample(eg->decay);
+      eg->egIncrement = MAX_EG / (float)stepsFull;
+      eg->nsteps = stepsFull * (eg->egval / MAX_EG);
+      break;
+    case release:
+      // eg->stage = done;
+      break;
+    case done:
+      break;
+  }
+}
+
+void _eg_release(EG *e) {
+  e->stage = sustain;
+  advanceStage(e);
+}
+
+void eg_init(EG *e) { e->attack = -12000; }
+
+spinner *newSpinner(int ch) {
   spinner *x = allocate_sp();
   x->outputf = &outputs[ch * RENDQ * 2];
   x->inputf = silence;
   x->channelId = ch;
+  x->lpf.z1 = x->lpf.z2 = 0;
 
   return x;
 }
 
-void trigger_release(spinner *x)
-{
+void trigger_release(spinner *x) {
   _eg_release(&x->voleg);
   _eg_release(&x->modeg);
   // if (x->zone->SampleModes > 1)
@@ -57,8 +255,7 @@ void trigger_release(spinner *x)
   //   x->is_looping = 0;
   // }
 }
-void reset(spinner *x)
-{
+void reset(spinner *x) {
   x->position = 0;
   x->stride = .0f;
   x->fract = 0.0f;
@@ -75,15 +272,13 @@ void reset(spinner *x)
   x->active_dynamics_flag = 0;
 }
 
-void set_midi_cc_val(int channel, int metric, int val)
-{
+void set_midi_cc_val(int channel, int metric, int val) {
   midi_cc_vals[channel * 128 + metric] = (char)(val & 0x7f);
 }
 
-float trigger_attack(spinner *x, uint32_t key, uint32_t velocity)
-{
 #define ccval(eff) midi_cc_vals[x->channelId * 128 + eff]
 
+float trigger_attack(spinner *x, uint32_t key, uint32_t velocity) {
   x->velocity = (unsigned char)velocity;
   x->position = 0;
   x->fract = 0.0f;
@@ -112,8 +307,7 @@ float trigger_attack(spinner *x, uint32_t key, uint32_t velocity)
 
   eg = &x->modeg;
   eg->stage = init;
-  if (ccval(TML_BANK_SELECT_MSB) > 0)
-  {
+  if (ccval(TML_BANK_SELECT_MSB) > 0) {
     x->is_looping = 0;
   }
 
@@ -138,26 +332,25 @@ float trigger_attack(spinner *x, uint32_t key, uint32_t velocity)
   advanceStage(&x->voleg);
   advanceStage(&x->modeg);
 
+#define effect_floor(v) v <= -12000 ? 0 : calcp2over1200(v)
   x->lfo1_pitch = effect_floor(x->zone->ModLFO2Pitch);
   x->lfo1_volume = effect_floor(x->zone->ModLFO2Vol);
   x->lfo2_pitch = effect_floor(x->zone->VibLFO2Pitch);
   x->modeg_pitch = effect_floor(x->zone->ModEnv2Pitch);
   x->modeg_fc = effect_floor(x->zone->ModEnv2FilterFc);
   x->lfo1_fc = effect_floor(x->zone->ModLFO2FilterFc);
-
   x->modlfo.delay = timecent2sample(x->zone->ModLFODelay);
   x->vibrlfo.delay = timecent2sample(x->zone->ModLFODelay);
   LFO_set_frequency(&x->modlfo, x->zone->ModLFOFreq);
   LFO_set_frequency(&x->vibrlfo, x->zone->VibLFOFreq);
   x->initialFc = x->zone->FilterFc;
-  x->initialQ = p10over200[x->zone->FilterQ + 1400];
-
-  new_lpf(&x->lpf, x->zone->FilterFc / SAMPLE_RATE, x->initialQ);
-
+  x->initialQ = p10over200[x->zone->FilterQ];
+  x->lpf.QInv = 1.0 / p10over200[x->zone->FilterQ];
+  new_lpf(&x->lpf, x->zone->FilterFc / SAMPLE_RATE);
   return x->stride;
 };
-void set_spinner_input(spinner *x, pcm_t *pcm)
-{
+
+void set_spinner_input(spinner *x, pcm_t *pcm) {
   x->loopStart = pcm->loopstart;
   x->loopEnd = pcm->loopend;
   x->inputf = pcm->data;
@@ -166,18 +359,14 @@ void set_spinner_input(spinner *x, pcm_t *pcm)
   x->position = 0;
 }
 
-float calc_pitch_diff_log(zone_t *z, pcm_t *pcm, unsigned char key)
-{
+float calc_pitch_diff_log(zone_t *z, pcm_t *pcm, unsigned char key) {
   short rt = z->OverrideRootKey > -1 ? z->OverrideRootKey : pcm->originalPitch;
-  float smpl_rate = rt * 100.f + z->CoarseTune * 100.f + z->FineTune;
-  float diff = key * 100.f - smpl_rate;
-  diff += ((pcm->sampleRate - SAMPLE_RATE) / 40.96f);
-  return diff;
+  float inputTS = rt * 100.0f + z->CoarseTune * 100.0f + (float)z->FineTune;
+  return (key * 100 - inputTS) / 1200.f;
 }
 #define ccval(eff) midi_cc_vals[x->channelId * 128 + eff]
 
-void set_spinner_zone(spinner *x, zone_t *z)
-{
+void set_spinner_zone(spinner *x, zone_t *z) {
   pcm_t *pcm = &pcms[z->SampleId];
   set_spinner_input(x, pcm);
   x->zone = z;
@@ -192,8 +381,7 @@ void set_spinner_zone(spinner *x, zone_t *z)
   x->sampleLength += z->EndAddrOfs + (z->EndAddrCoarseOfs << 15);
 }
 
-void _spinblock(spinner *x, int n, int blockOffset)
-{
+void _spinblock(spinner *x, int n, int blockOffset) {
   double db, dbInc;
   float stride = 1.0f;
   float pdiff = x->pitch_dff_log;
@@ -216,8 +404,7 @@ void _spinblock(spinner *x, int n, int blockOffset)
   kRateCB += (float)x->zone->Attenuation;
   kRateCB += midi_volume_log10(ccval(TML_VOLUME_MSB));
   kRateCB += midi_volume_log10(midi_cc_vals[ch * 128 + TML_EXPRESSION_MSB]);
-  if (x->voleg.stage < decay)
-    kRateCB += midi_volume_log10(x->velocity);
+  if (x->voleg.stage < decay) kRateCB += midi_volume_log10(x->velocity);
 
   double panLeft = panleftLUT[midi_cc_vals[ch * 128 + TML_PAN_MSB]];
 
@@ -235,38 +422,33 @@ void _spinblock(spinner *x, int n, int blockOffset)
   float Q = x->initialQ;
   short initFc = x->initialFc;
 
-  for (int i = 0; i < n; i++)
-  {
-    db = volEgOut[i]; //+ lfo1_volume * lfo1Out[i];
+  for (int i = 0; i < n; i++) {
+    db = volEgOut[i];  //+ lfo1_volume * lfo1Out[i];
     pdiff += lfo1Out[i] * lfo1_pitch + modEgOut[i] * modeg_pitch +
              lfo2Out[i] * lfo2_pitch;
 
     stride = calcp2over1200(pdiff);
 
     fract = fract + stride;
-    while (fract >= 1.0f)
-    {
+    while (fract >= 1.0f) {
       position++;
       fract -= 1.0f;
     }
-    if (position >= x->loopEnd && isLooping > 0)
-      position -= looplen;
+    if (position >= x->loopEnd && isLooping > 0) position -= looplen;
 
     outputf = lerp(x->inputf[position], x->inputf[position + 1], fract);
     tfc = initFc + modeg_fc * modEgOut[i] + x->lfo1_fc * lfo1Out[i];
 
-    if (position > nsamples)
-    {
+    if (position > nsamples) {
       position = 0;
       outputf = 0.0;
       x->voleg.stage = done;
     }
     outputf = applyCentible(outputf, (short)(db + kRateCB));
 
-    if (tfc > .5)
-    {
+    if (tfc > .5) {
       fchertz = timecent2hertz(tfc) / SAMPLE_RATE;
-      new_lpf(&lpf, fchertz, Q);
+      new_lpf(&lpf, fchertz);
       outputf = calc_lpf(&lpf, outputf);
     }
     output_L[i] = applyCentible(outputf, panLeft);
@@ -277,19 +459,16 @@ void _spinblock(spinner *x, int n, int blockOffset)
   x->stride = stride;
 }
 
-int spin(spinner *x, int n)
-{
+int spin(spinner *x, int n) {
   _spinblock(x, 64, 0);
 
   _spinblock(x, 64, 64);
 
-  if (x->voleg.egval < -1440.f)
-  {
+  if (x->voleg.egval < -1440.f) {
     x->voleg.stage = done;
     return 0;
   }
-  if (x->voleg.stage == done)
-  {
+  if (x->voleg.stage == done) {
     return 0;
   }
   return 1;
@@ -302,37 +481,73 @@ EG *get_mod_eg(spinner *x) { return &x->modeg; }
 float *get_sp_output(spinner *x) { return x->outputf; }
 int get_sp_channel_id(spinner *x) { return x->channelId; }
 
-void gm_reset()
-{
-  for (int idx = 0; idx < nmidiChannels; idx++)
-  {
+void gm_reset() {
+  for (int idx = 0; idx < nmidiChannels; idx++) {
     midi_cc_vals[idx * num_cc_list + TML_VOLUME_MSB] = 100;
     midi_cc_vals[idx * num_cc_list + TML_PAN_MSB] = 64;
     midi_cc_vals[idx * num_cc_list + TML_EXPRESSION_MSB] = 127;
     if (idx == def_drum_c)
       midi_cc_vals[idx * num_cc_list + TML_BANK_SELECT_MSB] = 128;
   }
-  for (int i = 0; i < nchannels; i++)
-    reset(&sps[i]);
+  for (int i = 0; i < nchannels; i++) reset(&sps[i]);
 }
-// #include <math.h>
-// #include <stdio.h>
-// #include <stdlib.h>
+#define TEST_ENV 1
+#ifdef TEST_ENV
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define BIT32_NORMALIZATION 4294967296.0f
 
-// int main() {
-// FILE* fd = fopen("/dev/stdout", "rb");
-// FILE* fo = fopen("ggg.pcm", "w");
-//   float* f;
-//   float* o;
-//   if (!fd) return 1;
-//   float Q = .50f;
-//   new_lpf(b, .5f, .50f);
+int main() {
+#define ts2hz(ts) 8.176f * powf(2.0f, (float)ts / 1200.0)
 
-//   while (!feof(fd)) {
-//     fread(f, sizeof(float), 1, fd);
-//     *o = calc_lpf(b, *f);
-//     fwrite(o, sizeof(float), 1, fo);
-//   }
+  float sample[4096] = {0.f};
+  for (int i = 0; i < 4096; i++) {
+    sample[i] = .5 * sinf((float)i * M_2_PI / 4096);
+    sample[i] = 2 * sinf(.5 * (float)i * M_2_PI / 4096);
+  }
+  spinner *x = newSpinner(0);
+  short FilterFc = 8000;
+  short Q = 410;
+  float lowpassFc = ts2hz(FilterFc) / SAMPLE_RATE;
+  x->lpf.QInv = 1.0 / powf(10.f, Q / 200.0);
+  new_lpf(&x->lpf, lowpassFc);
+  FILE *adc;
 
-//   return 0;
-// }
+  adc = popen("ffplay -loglevel debug -i pipe:0 -f f32le -ac 1 -ar 48000", "w");
+  uint32_t pinc = ts2hz(6900) * BIT32_NORMALIZATION / SAMPLE_RATE;
+  uint32_t wavetableBits = 12;
+  uint32_t nfract = 32 - wavetableBits;
+  uint32_t mask_fraction = (1L << nfract) - 1;
+  uint32_t mask_index = wavetableBits - 1;
+  float scalar_fractional = 4096.f / BIT32_NORMALIZATION;
+  uint32_t p = 0;
+  float o;
+
+  for (uint32_t i = 0; i < 48900 * 5; i++) {
+    uint32_t index = (uint32_t)(p >> nfract) & 0x0fff;
+    uint32_t index2 = (index + 1) & 0x0fff;
+    float g1 = scalar_fractional * (float)(p & mask_fraction);
+    float v = lerp(sample[index], sample[index2], g1);
+    o = calc_lpf(&x->lpf, v);
+    p += pinc;
+    // fwrite(&v, 4, 1, adc);
+    fwrite(&o, 4, 1, adc);
+    if (i % 10000 == 5000) {
+      lowpassFc += 100;
+      new_lpf(&x->lpf, lowpassFc);
+    }
+    if (i % 10000 == 0) {
+      lowpassFc -= 100;
+      new_lpf(&x->lpf, lowpassFc);
+    }
+  }
+  if (adc) fclose(adc);
+  // while (!feof(fd)) {
+  //   fread(f, sizeof(float), 1, fd);
+  //   *o = calc_lpf(&x->lpf, *f);
+  //   fwrite(o, sizeof(float), 1, fo);
+  // }
+  return 0;
+}
+#endif
--- spin/src/spin.h
+++ spin/src/spin.h
@@ -13,86 +13,24 @@ typedef unsigned int uint32_t;
 #define num_cc_list 128
 #define MAX_EG -1440.f
 #define SAMPLE_RATE 44100.0f
-#include "calc.h"
-#define def_drum_c 9
 
-#define modulo_s16f_inverse 1.0f / 32767.1f
-#define modulo_u16f (float)(((1 << 16) + .1f))
-extern float tanf(float t);
+typedef struct {
+  uint32_t loopstart, loopend, length, sampleRate, originalPitch;
+  float *data;
+} pcm_t;
 
-typedef struct
-{
+typedef struct {
+  float mod2volume, mod2pitch, mod2filter;
+} LFOEffects;
+
+typedef struct {
   unsigned short phase, delay;
   unsigned short phaseInc;
 } LFO;
 
-float dummy[666]; // backward compact hack
-
-float LFO_roll_out(LFO *lfo, unsigned int n, float *output)
-{
-  while (n--)
-  {
-    if (lfo->delay > 0)
-    {
-      lfo->delay--;
-      *output++ = 0.f;
-      continue;
-    }
-    else
-    {
-      lfo->phase += lfo->phaseInc;
-      *output++ = (float)(((short)lfo->phase) * modulo_s16f_inverse);
-    }
-  }
-  return *output;
-}
-
-void LFO_set_frequency(LFO *lfo, short ct)
-{
-  double freq = timecent2hertz(ct);
-  lfo->phaseInc = (unsigned short)(modulo_u16f * freq / SAMPLE_RATE);
-}
-
-float LFO_roll(LFO *lfo, unsigned int n)
-{
-  if (lfo->delay > n)
-  {
-    lfo->delay -= n;
-    return 0.0f;
-  }
-  else
-  {
-    n -= lfo->delay;
-    lfo->delay = 0;
-  }
-  while (n--)
-    lfo->phase += lfo->phaseInc;
-  float lfoval = (float)(((short)lfo->phase) * modulo_s16f_inverse);
-  return lfoval;
-}
-
-#if !defined(fp12)
-#define fp12
-const int scale = 12;
-const int fraction_mask = (1 << scale) - 1;
-const int whole_mask = -1 ^ fraction_mask;
-const double scalar_multiple = (double)(1 << scale);
-#define double2fixed(x) (x * scalar_multiple)
-inline static double fixed2double(int x) { return x / scalar_multiple; }
-#define int2fixed(x) x << scale
-#define fixed2int(x) x >> scale
-inline static double get_fraction(int x)
-{
-  return fixed2double(x & fraction_mask);
-}
-#define fixed_floor(x) x >> scale
-
-#endif // fp12
-
-enum eg_stages
-{
-  inactive = 0, //
-  init = 1,     // this is for key on message sent and will go next render cycle
+enum eg_stages {
+  inactive = 0,  //
+  init = 1,  // this is for key on message sent and will go next render cycle
   delay = 2,
   attack = 3,
   hold = 4,
@@ -101,166 +39,17 @@ enum eg_stages
   release = 7,
   done = 99
 };
-typedef struct
-{
+typedef struct {
   float egval, egIncrement;
   int hasReleased, stage, nsteps;
   short delay, attack, hold, decay, sustain, release, pad1, pad2;
-  int progress, progressInc; // add prog scale to use LUT
+  int progress, progressInc;  // add prog scale to use LUT
 } EG;
 
-void advanceStage(EG *eg);
-float update_eg(EG *eg, int n);
-
-void eg_roll(EG *eg, int n, float *output)
-{
-  while (n-- && eg->nsteps--)
-  {
-    if (eg->stage == attack)
-    {
-      int lut_index = fixed_floor(eg->progress);
-      double frag = get_fraction(eg->progress);
-      double f1 = att_db_levels[lut_index], f2 = att_db_levels[lut_index + 1];
-      eg->egval = lerpd(f1, f2, frag);
-    }
-    else
-    {
-      eg->egval += eg->egIncrement;
-    }
-    *output++ = eg->egval;
-  }
-  if (eg->egval > 0)
-    eg->egval = 0.0f;
-  if (eg->nsteps <= 7)
-    advanceStage(eg);
-}
-/**
- * advances envelope generator by n steps..
- * shift to next stage and advance the remaining n steps
- * if necessary
- *
- */
-float update_eg(EG *eg, int n)
-{
-  while (n--)
-  {
-    eg->egval += eg->egIncrement;
-    eg->nsteps--;
-  }
-  if (eg->nsteps <= 7)
-    advanceStage(eg);
-  if (eg->egval > 0)
-    eg->egval = 0.0f;
-  return eg->egval;
-}
-
-void advanceStage(EG *eg)
-{
-  switch (eg->stage)
-  {
-  case inactive:
-    eg->stage++;
-    return;
-  case init:
-    eg->stage = delay;
-    eg->egval = MAX_EG;
-
-    if (eg->delay > -12000)
-    {
-      eg->egval = MAX_EG;
-      eg->nsteps = timecent2sample(eg->delay);
-      eg->egIncrement = 0.0f;
-      break;
-    }
-  case delay:
-    eg->stage = attack;
-    eg->egval = MAX_EG;
-    eg->nsteps = timecent2sample(eg->attack);
-    eg->progress = double2fixed(0);
-    eg->progressInc = double2fixed(255.0 / (double)eg->nsteps);
-    break;
-  case attack:
-    eg->stage = hold;
-    eg->egval = 0.0f;
-    eg->nsteps = timecent2sample(eg->hold);
-    eg->egIncrement = 0.0f;
-
-    break;
-  case hold: /** TO DECAY */
-    eg->stage = decay;
-
-    if (eg->sustain > 0)
-    {
-      eg->nsteps = timecent2sample(eg->decay);
-      eg->egIncrement = (MAX_EG + eg->sustain / 1000) / eg.
-      
-    }
-    else
-    {
-      eg->nsteps = 0;
-    }
-    break;
-
-  case decay: // headsing to released;
-
-    /*
-    37 sustainVolEnv This is the decrease in level, expressed in centibels,
-    to which the Volume Envelope value ramps during the decay phase. For the
-    Volume Envelope, the sustain level is best expressed in centibels of
-    attenuation from full scale. A value of 0 indicates the sustain level is
-    full level; this implies a zero duration of decay phase regardless of
-    decay time. A positive value indicates a decay to the corresponding
-    level. Values less than zero are to be interpreted as zero;
-    conventionally 1000 indicates full attenuation. For example, a sustain
-    level which corresponds to an absolute value 12dB below of peak would be
-    120.*/
-    eg->stage = sustain;
-    eg->egIncrement = 0.0f;
-    eg->nsteps = 0xfffff;
-    break;
-
-    // sustain = % decreased during decay
-
-  case sustain:
-    eg->stage = release;
-    int stepsFull =
-        timecent2sample(eg->release); //+ timecent2sample(eg->decay);
-    eg->egIncrement = MAX_EG / (float)stepsFull;
-    eg->nsteps = stepsFull * (eg->egval / MAX_EG);
-    break;
-  case release:
-    // eg->stage = done;
-    break;
-  case done:
-    break;
-  }
-}
-
-void _eg_release(EG *e)
-{
-  e->stage = sustain;
-  advanceStage(e);
-}
-
-void eg_init(EG *e) { e->attack = -12000; }
-
-typedef struct
-{
-  uint32_t loopstart, loopend, length, sampleRate, originalPitch;
-  float *data;
-} pcm_t;
-
-typedef struct
-{
-  float mod2volume, mod2pitch, mod2filter;
-} LFOEffects;
-
-typedef struct
-{
+typedef struct {
   uint8_t lo, hi;
-} rangesType; //  Four-character code
-typedef struct
-{
+} rangesType;  //  Four-character code
+typedef struct {
   unsigned short StartAddrOfs, EndAddrOfs, StartLoopAddrOfs, EndLoopAddrOfs,
       StartAddrCoarseOfs;
   short ModLFO2Pitch, VibLFO2Pitch, ModEnv2Pitch, FilterFc, FilterQ,
@@ -279,8 +68,7 @@ typedef struct
       OverrideRootKey, Dummy;
 } zone_t;
 
-enum grntypes
-{
+enum grntypes {
   StartAddrOfs,
   EndAddrOfs,
   StartLoopAddrOfs,
@@ -343,13 +131,11 @@ enum grntypes
   Dummy
 };
 /* this holds the data required to update samples thru a filter */
-typedef struct
-{
+typedef struct {
   double QInv, a0, a1, b1, b2, z1, z2;
 } Biquad;
 
-typedef struct
-{
+typedef struct {
   float *inputf, *outputf;
   unsigned char channelId, key, velocity, p1, p2, p3, p4, p5;
   uint32_t position, loopStart, loopEnd;
@@ -368,45 +154,31 @@ typedef struct
 
 } spinner;
 
+#if !defined(fp12)
+#define fp12
+const int scale = 12;
+const int fraction_mask = (1 << scale) - 1;
+const int whole_mask = -1 ^ fraction_mask;
+const double scalar_multiple = (double)(1 << scale);
+#define double2fixed(x) (x * scalar_multiple)
+inline static double fixed2double(int x) { return x / scalar_multiple; }
+#define int2fixed(x) x << scale
+#define fixed2int(x) x >> scale
+inline static double get_fraction(int x) {
+  return fixed2double(x & fraction_mask);
+}
+#define fixed_floor(x) x >> scale
+
+#endif  // fp12
+
 void set_spinner_zone(spinner *x, zone_t *z);
 spinner *newSpinner(int ch);
 void reset(spinner *x);
 int spin(spinner *x, int n);
 float *spOutput(spinner *x);
 
-void scaleTc(EG *eg, unsigned int pcmSampleRate)
-{
-  float scaleFactor = SAMPLE_RATE / (float)pcmSampleRate;
-  eg->attack *= scaleFactor;
-  eg->delay *= scaleFactor;
-  eg->decay *= scaleFactor;
-  eg->release *= scaleFactor;
-  eg->hold *= scaleFactor;
-}
-void init_vol_eg(EG *eg, zone_t *z, unsigned int pcmSampleRate)
-{
-  float scaleFactor = SAMPLE_RATE / (float)pcmSampleRate;
-  eg->attack = z->VolEnvAttack * scaleFactor;
-  eg->delay = z->VolEnvDelay * scaleFactor;
-  eg->decay = z->VolEnvDecay * scaleFactor;
-  eg->release = z->VolEnvRelease * scaleFactor;
-  eg->hold = z->VolEnvHold * scaleFactor;
-  eg->sustain = z->VolEnvSustain * scaleFactor;
-  eg->stage = init;
-  eg->nsteps = 0;
-}
-void init_mod_eg(EG *eg, zone_t *z, unsigned int pcmSampleRate)
-{
-  eg->attack = z->ModEnvAttack;
-  eg->delay = z->ModEnvDelay;
-  eg->decay = z->ModEnvDecay;
-  eg->release = z->ModEnvRelease;
-  eg->hold = z->ModEnvHold;
-  eg->sustain = z->ModEnvSustain;
-}
 // Midi controller numbers
-enum TMLController
-{
+enum TMLController {
   TML_BANK_SELECT_MSB,
   TML_MODULATIONWHEEL_MSB,
   TML_BREATH_MSB,
@@ -480,25 +252,5 @@ enum TMLController
   TML_POLY_OFF,
   TML_POLY_ON
 };
-
-void new_lpf(Biquad *biq, float fc, float Q)
-{
-  double K = tanf(3.1415f * fc);
-  double KK = K * K;
-  double norm = 1 / (1 + K / Q + KK);
-  biq->QInv = 1.0 / Q;
-  biq->a0 = KK * norm;
-  //
-  biq->a1 = 2 * biq->a0;
-  biq->b1 = 2 * (KK - 1) * norm;
-  biq->b2 = (1 - K * biq->QInv + KK) * norm;
-}
-float calc_lpf(Biquad *b, double In)
-{
-  double Out = In * b->a0 + b->z1;
-  b->z1 = In * b->a1 + b->z2 - b->b1 * Out;
-  b->z2 = In * b->a0 - b->b2 * Out;
-  return (float)Out;
-}
 
 #endif
