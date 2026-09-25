397a08b994cbb46ad27a6e606091f4bdaa8b0d64
Yisheng Jiang <yishengjiang@yahoo.com>
2026-09-25 16:58:11 +0000

    Load the low-pass worklet from lpf/lpf-proc.js
    The page asked for /lpf-proc.js, which 404s. The processor is in lpf/.

--- lpf/lpf.js
+++ lpf/lpf.js
@@ -4,12 +4,7 @@ import { wasmbin } from "./lpf.wasm.js";
 export class LowPassFilterNode extends AudioWorkletNode {
   static default_params = {FilterFC: 13500, FilterQ: 0};
   static async init(ctx) {
-    try {
-      await ctx.audioWorklet
-        .addModule("lpf-proc.js");
-    } catch (e) {
-      await ctx.audioWorklet.addModule('lpf/lpf-proc.js');
-    }
+    await ctx.audioWorklet.addModule("lpf/lpf-proc.js");
   }
   constructor(ctx, options = {}) {
     super(ctx, "lpf-proc", {
